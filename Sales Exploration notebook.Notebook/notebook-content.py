2cae60c1ed38b40d4c04f0bf432dd5cb489afc53
nrkaurs <130146572+nrkaurs@users.noreply.github.com>
2026-09-05 02:02:21 -0700

    Committing 3 items from workspace f9495467-2382-4250-84e8-82c09c945816

---
+++ Sales Exploration notebook.Notebook/notebook-content.py
@@ -0,0 +1,280 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "ec009e18-830a-439d-923a-03e4c0c77d6c",
+# META       "default_lakehouse_name": "lh1",
+# META       "default_lakehouse_workspace_id": "f9495467-2382-4250-84e8-82c09c945816",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "ec009e18-830a-439d-923a-03e4c0c77d6c"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# # Sales order data exploration
+# Use this notebook to explore sales order data
+
+# CELL ********************
+
+from pyspark.sql.types import *
+
+orderSchema = StructType([
+   StructField("SalesOrderNumber", StringType()),
+   StructField("SalesOrderLineNumber", IntegerType()),
+   StructField("OrderDate", DateType()),
+   StructField("CustomerName", StringType()),
+   StructField("Email", StringType()),
+   StructField("Item", StringType()),
+   StructField("Quantity", IntegerType()),
+   StructField("UnitPrice", FloatType()),
+   StructField("Tax", FloatType())
+])
+
+df = spark.read.format("csv").schema(orderSchema).load("Files/orders/*.csv")
+
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+customers = df['CustomerName', 'Email'] 
+#customers = df.select("CustomerName", "Email")
+print(customers.count())
+print(customers.distinct().count())
+
+display(customers.distinct())
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+customers = df.select("CustomerName", "Email").where(df['Item']=='Road-250 Red, 52')
+print(customers.count())
+print(customers.distinct().count())
+
+display(customers.distinct())
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+productSales = df.select("Item", "Quantity").groupBy("Item").sum()
+
+display(productSales)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import *
+
+yearlySales = df.select(year(col("OrderDate")).alias("Year")).groupBy("Year").count().orderBy("Year")
+
+display(yearlySales)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import *
+
+# Create Year and Month columns
+transformed_df = df.withColumn("Year", year(col("OrderDate"))).withColumn("Month", month(col("OrderDate")))
+
+# Create the new FirstName and LastName fields
+transformed_df = transformed_df.withColumn("FirstName", split(col("CustomerName"), " ").getItem(0)).withColumn("LastName", split(col("CustomerName"), " ").getItem(1))
+
+# Filter and reorder columns
+transformed_df = transformed_df["SalesOrderNumber", "SalesOrderLineNumber", "OrderDate", "Year", "Month", "FirstName", "LastName", "Email", "Item", "Quantity", "UnitPrice", "Tax"]
+
+# Display the first five orders
+display(transformed_df.limit(5))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+transformed_df.write.mode("overwrite").parquet('Files/transformed_data/orders')
+
+print ("Transformed data saved!")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+orders_df = spark.read.format("parquet").load("Files/transformed_data/orders")
+display(orders_df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+orders_df.write.partitionBy("Year","Month").mode("overwrite").parquet("Files/partitioned_data")
+
+print ("Transformed data saved!")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+orders_2021_df = spark.read.format("parquet").load("Files/partitioned_data/Year=2021/Month=[123]")
+
+display(orders_2021_df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Create a new table
+df.write.format("delta").saveAsTable("salesorders")
+
+# Get the table description
+spark.sql("DESCRIBE EXTENDED salesorders").show(truncate=False)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM salesorders LIMIT 1000")
+
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC SELECT YEAR(OrderDate) AS OrderYear,
+# MAGIC       SUM((UnitPrice * Quantity) + Tax) AS GrossRevenue
+# MAGIC FROM salesorders
+# MAGIC GROUP BY YEAR(OrderDate)
+# MAGIC ORDER BY OrderYear;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC SELECT * FROM salesorders
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+sqlQuery = "SELECT CAST(YEAR(OrderDate) AS CHAR(4)) AS OrderYear, \
+               SUM((UnitPrice * Quantity) + Tax) AS GrossRevenue, \
+               COUNT(DISTINCT SalesOrderNumber) AS YearlyCounts \
+           FROM salesorders \
+           GROUP BY CAST(YEAR(OrderDate) AS CHAR(4)) \
+           ORDER BY OrderYear"
+df_spark = spark.sql(sqlQuery)
+df_spark.show()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from matplotlib import pyplot as plt
+
+# matplotlib requires a Pandas dataframe, not a Spark one
+df_sales = df_spark.toPandas()
+
+# Create a bar plot of revenue by year
+plt.bar(x=df_sales['OrderYear'], height=df_sales['GrossRevenue'])
+
+# Display the plot
+plt.show()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
